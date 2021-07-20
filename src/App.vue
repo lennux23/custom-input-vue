0a4a990352e8baf4d4b9194c119dcb2f4092d095
lennux23 <alexochihua@hotmail.com>
2021-07-20 17:11:49 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--e-input :valorDeEntrada="numeroDos" @estoyActualizandoAlpadre="actualizarValorEnApp"-->
+    <!--e-input
+      v-model="numeroDos"
+      @estoyActualizandoAlpadre="actualizarValorEnApp"
+    /-->
+    <e-input v-model="numeroDos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EInput from "./components/EInput.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    EInput,
+  },
+  data() {
+    return {
+      numeroDos: "2",
+    };
+  },
+  methods: {
+    actualizarValorEnApp(valorentrada) {
+      console.log("actualizarValorEnApp", valorentrada);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/EInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input :value="maskValue" @input="onChangeInput" />
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "valorDeEntrada",
+    event: "estoyActualizandoAlpadre",
+  },
+  props: {
+    valorDeEntrada: {
+      type: String,
+    },
+  },
+  methods: {
+    onChangeInput(event) {
+      this.$emit("estoyActualizandoAlpadre", event.target.value);
+    },
+  },
+  computed: {
+    maskValue() {
+      return `$ ${this.valorDeEntrada}`;
+    },
+  },
+};
+</script>
